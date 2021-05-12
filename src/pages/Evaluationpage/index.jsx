f24128ccf519353ebacd3e7b6ab9dc1eaa396836
Raksani <raksani.k@ku.th>
2021-05-13 01:35:55 +0700

    Add evaluation page

--- src/pages/Evaluationpage/index.jsx
+++ src/pages/Evaluationpage/index.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import Link from 'react-router-dom'
+import React, { Component } from 'react'
+import { Card, Space, Button, Rate } from 'antd'
+import NavBar from '../../components/NavBar'
 import './styled.css'
 import './../../index.css'
 
-const EvaluationPage = () => {
-  return (
-    <div className="blue-container">
-    </div>
-  )
-}
+export default class EvaluationPage extends Component {
+  state = {
+    id: 1,
+    name: 'Raksani Khunamas',
+    position: 'Software Engineering Intern',
+    score: 0
+  }
 
-export default EvaluationPage;
+  handleChange = (newScore) => {
+    this.setState({ score: newScore })
+  }
+
+  render() {
+    return (
+      <div className="blue-container">
+        <NavBar />
+        <Card className="card">
+          <Space direction="vertical" style={{ width: '100%' }}>
+            <h2>#{this.state.id}</h2>
+            <p>Employee Name: {this.state.name}</p>
+            <p>Position: {this.state.position}</p>
+            <p>Score: {this.state.score}</p>
+            <Rate allowHalf onChange={this.handleChange} />
+            <div className="btn-submit">
+              <Button type="primary" block>
+                Submit
+              </Button>
+            </div>
+          </Space>
+        </Card>
+      </div>
+    )
+  }
+}
