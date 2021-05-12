f5a7a49556332cee9fa9da44f2cbaa9812b44a06
Raksani <raksani.k@ku.th>
2021-05-13 01:36:07 +0700

    Add NavBar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styled.css'
+import './../../index.css'
+
+const NavBar = () => {
+  return (
+    <div className="header">
+      <div className="header text">Evaluation</div>
+    </div>
+  )
+}
+
+export default NavBar
