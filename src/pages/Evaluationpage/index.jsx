2edf5da6fce4e91661d9335630988a4bf101c33e
Raksani <raksani.k@ku.th>
2021-05-13 02:54:39 +0700

    Add card list

--- src/pages/Evaluationpage/index.jsx
+++ src/pages/Evaluationpage/index.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
-import { Card, Space, Button, Rate } from 'antd'
+import { Card, Col, Row, Space, Button, Rate } from 'antd'
 import NavBar from '../../components/NavBar'
 import './styled.css'
 import './../../index.css'
 
 export default class EvaluationPage extends Component {
   state = {
-    id: 1,
-    name: 'Raksani Khunamas',
-    position: 'Software Engineering Intern',
     score: 0
   }
 
@@ -17,23 +14,53 @@ export default class EvaluationPage extends Component {
   }
 
   render() {
+    const employees = [
+      {
+        id: 1,
+        name: 'Raksani Khunamas',
+        position: 'Software Engineer Intern'
+      },
+      {
+        id: 2,
+        name: 'Thanakrit Daoruang',
+        position: 'Backend Developer'
+      },
+      {
+        id: 3,
+        name: 'Narisa Mint',
+        position: 'Senior Frontend Developer'
+      },
+      {
+        id: 4,
+        name: 'Narisa Mint',
+        position: 'Senior Frontend Developer'
+      }
+    ]
     return (
       <div className="blue-container">
         <NavBar />
-        <Card className="card">
-          <Space direction="vertical" style={{ width: '100%' }}>
-            <h2>#{this.state.id}</h2>
-            <p>Employee Name: {this.state.name}</p>
-            <p>Position: {this.state.position}</p>
-            <p>Score: {this.state.score}</p>
-            <Rate allowHalf onChange={this.handleChange} />
-            <div className="btn-submit">
-              <Button type="primary" block>
-                Submit
-              </Button>
-            </div>
-          </Space>
-        </Card>
+        <div className="card-container">
+          <Row gutter={[16, 16]}>
+            {employees.map((employee) => (
+              <Col span={8}>
+                <Card className="card">
+                  <Space direction="vertical" style={{ width: '100%' }}>
+                    <h2>#{employee.id}</h2>
+                    <p>Employee Name: {employee.name}</p>
+                    <p>Position: {employee.position}</p>
+                    <p>Score: {this.state.score}</p>
+                    <Rate allowHalf onChange={this.handleChange} />
+                    <div className="btn-submit">
+                      <Button type="primary" block>
+                        Submit
+                      </Button>
+                    </div>
+                  </Space>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
       </div>
     )
   }
