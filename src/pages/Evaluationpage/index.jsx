f05666fe3bf0e3d3b3cc7ba9ab3f7ff6b8d1b58e
Raksani <raksani.k@ku.th>
2021-05-12 23:57:45 +0700

    Initial setup evaluation page

---
+++ src/pages/Evaluationpage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'react-router-dom'
+import './styled.css'
+import './../../index.css'
+
+const EvaluationPage = () => {
+  return (
+    <div className="regis-container">
+    </div>
+  )
+}
+
+export default EvaluationPage;
